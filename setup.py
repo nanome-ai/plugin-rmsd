2caadfaf4babcb017bce1679c5e2d49a8877e082
Ramji <jeremie.rahm@epitech.eu>
2019-07-19 10:31:35 -0700

    Bump version: 0.0.3 → 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 setup(
 	name = 'nanome-rmsd',
 	packages=find_packages(),
-	version = '0.0.3',
+	version = '0.1.0',
 	license='MIT',
 	description = 'Nanome Plugin for RMSD alignment',
 	long_description = README,
